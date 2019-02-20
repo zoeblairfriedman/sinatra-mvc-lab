e8b3ab15f43b33dfd990cc97db8afc4af66f991c
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-20 18:00:50 -0500

    Reverting test change. According to https://web.ics.purdue.edu/~morelanj/RAO/prepare2.html the pig latin version of 'Stream' should be 'eamStray' rather than 'ayStream'. As this is also breaking tests I am reverting directly rather than through a pull request.
    This reverts commit 5e513d113bf25507b6642c4d8bf92591ad99e067.

--- spec/sinatra_mvc_lab_spec.rb
+++ spec/sinatra_mvc_lab_spec.rb
@@ -56,7 +56,7 @@ describe "Pig Latinizer App" do
     end
 
     it "displays the pig latinized phrase upon form submission" do
-      expect(last_response.body).to include("eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay ayStream andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
+      expect(last_response.body).to include("eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
     end
   end
 
