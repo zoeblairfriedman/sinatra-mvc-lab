415ece79628a58f107324b9a59a82f52d04ebf8d
github username <github email address>
2020-04-16 18:47:42 -0700

    Refactor piglatinize method

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,37 +1,26 @@
 class PigLatinizer
 
   def piglatinize(input_str)
-    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    input_str.split(" ").map{ |word| piglatinize_word(word)}.join(" ")
   end
 
   private
 
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
+  def vowel?(char)
+    char.match(/[aAeEiIoOuU]/)
   end
 
   def piglatinize_word(word)
     # word starts with vowel
-    if !consonant?(word[0])
-      word = word + "w"
-    # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
+    if vowel?(word[0])
+      word + "way"
     else
-      word = word.slice(1..-1) + word.slice(0)
+    # word starts with one or more consonants
+      vowel_index = word.index(/[aAeEiIoOuU]/)
+      consonants = word.slice(0..vowel_index-1)
+      word_leftover = word.slice(vowel_index..word.length)
+      word_leftover + consonants + "ay"
     end
-    word << "ay"
   end
-
-  def piglatinize_sentence(sentence)
-    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
-  end
-
-
-
 
 end
